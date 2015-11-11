622180d08f7457a1022b0e9bf3786b59278bf75d
slatequarry <michael.peter@gmail.com>
2015-11-11 17:48:02 +0100

    set application title and style

--- OSMBrowser.dpr
+++ OSMBrowser.dpr
@@ -4,13 +4,17 @@ uses
   Vcl.Forms,
   uOSMBrowser in 'uOSMBrowser.pas' {frmOSMbrowser},
   uOSM in 'uOSM.pas',
-  uConfigDialog in 'uConfigDialog.pas' {frmConfig};
+  uConfigDialog in 'uConfigDialog.pas' {frmConfig},
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.Title := 'OSM Browser';
+  TStyleManager.TrySetStyle('Aqua Light Slate');
   Application.CreateForm(TfrmOSMbrowser, frmOSMbrowser);
   Application.Run;
 end.
